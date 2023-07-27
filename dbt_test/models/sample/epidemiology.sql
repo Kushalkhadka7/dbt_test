cb28a30bb8deb19ebe92f57a95ded88c696c12d6
kushalkhadka7 <kushalkhadka@lftechnology.com>
2023-07-27 21:10:27 +0545

    add models

--- dbt_test/models/sample/epidemiology.sql
+++
@@ -1,9 +0,0 @@
-{{ config(materialized='table') }}
-
-with source_epidemiology as (
-  select * from {{ source('dbt_git', 'commits') }}
-),
-final as (
-  select * from source_epidemiology
-)
-SELECT * FROM final
